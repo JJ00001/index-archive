c604df227412e6065344259087bcb59def8f538c
Julien Guntermann <julienguntermann@web.de>
2024-10-03 19:04:04 +0200

    feat: add Company Index view with data table

---
+++ resources/js/Pages/Company/Index.vue
@@ -0,0 +1,29 @@
+<script setup>
+import LayoutWrapper from "@/Layouts/LayoutWrapper.vue";
+import DataTable from "primevue/datatable";
+import Column from 'primevue/column';
+
+defineProps({
+    companies: {
+        type: Array,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <layout-wrapper>
+        <data-table :value="companies" striped-rows>
+            <column field="ticker" header="Ticker"/>
+            <column field="name" header="Name"/>
+            <column field="market_datas" header="Gewichtung in %">
+                <template #body="slotProps">
+                    {{ slotProps.data.market_datas[0].weight }} %
+                </template>
+            </column>
+            <column field="sector.name" header="Branche"/>
+            <column field="country.name" header="Land"/>
+            <column field="exchange.name" header="Börsenplatz"/>
+        </data-table>
+    </layout-wrapper>
+</template>
