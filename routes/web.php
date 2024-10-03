6015fffac347b112235a06c3f9b35f322f2be966
Julien Guntermann <julienguntermann@web.de>
2024-10-03 19:02:44 +0200

    feat: add Company index, show routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Dashboard');
 });
+
+Route::resource('companies', CompanyController::class)->only(['index', 'show']);
