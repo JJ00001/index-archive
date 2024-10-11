ae640dc0c51c2b958f4b98f1a38d9766d5666213
Julien Guntermann <julienguntermann@web.de>
2024-10-11 08:27:29 +0200

    feat: add main encapsulating layout

---
+++ resources/js/Layouts/LayoutMain.vue
@@ -0,0 +1,20 @@
+<script setup>
+import LayoutWrapper from "@/Layouts/LayoutWrapper.vue";
+import Navbar from "@/Components/Navbar.vue";
+import Card from "primevue/card";
+</script>
+
+<template>
+    <layout-wrapper>
+        <div class="space-y-10">
+            <navbar class="w-full"/>
+            <div class="lg:w-10/12 mx-auto max-w-screen-2xl">
+                <Card>
+                    <template #content>
+                        <slot/>
+                    </template>
+                </Card>
+            </div>
+        </div>
+    </layout-wrapper>
+</template>
