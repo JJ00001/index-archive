d90a36fc982f81a8913a186aa957543577e8b2ff
Julien Guntermann <julienguntermann@web.de>
2024-10-31 12:18:08 +0100

    refactor: move logic to CompanyLogoService

--- app/Jobs/FetchCompanyLogo.php
+++ app/Jobs/FetchCompanyLogo.php
@@ -2,14 +2,11 @@
 
 namespace App\Jobs;
 
+use App\Http\Services\CompanyLogoService;
 use App\Models\Company;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Illuminate\Http\Client\ConnectionException;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 
 class FetchCompanyLogo implements ShouldQueue, ShouldBeUnique
 {
@@ -33,24 +30,15 @@ class FetchCompanyLogo implements ShouldQueue, ShouldBeUnique
      */
     public function handle(): void
     {
-        try {
-            $response = Http::withHeader('X-Api-Key', env('API_NINJA_API_KEY'))
-                ->accept('application/json')
-                ->get('https://api.api-ninjas.com/v1/logo?ticker=' . $this->company->ticker);
+        $companyLogoService = new CompanyLogoService($this->company);
 
-            $logoURL = $response->json()[0]['image'] ?? null;
+        $logoUrl = $companyLogoService->fetchLogo();
 
-            if ($logoURL) {
-                $response = Http::get($logoURL);
-                $logo = $response->body();
-                $logoPathInStorage = 'logos/' . $this->company->ticker . '.png';
-                Storage::disk('public')->put($logoPathInStorage, $logo);
-                $this->company->update(['logo' => $logoPathInStorage]);
-
-                Log::info('Logo fetching complete: ' . $this->company->name);
-            }
-        } catch (ConnectionException $e) {
-            Log::info($e->getMessage());
-        }
+        dd($logoUrl);
+//        if ($logoUrl) {
+//            $companyLogoService->storeLogo($logoUrl);
+//        } else {
+//            dd('No logo found');
+//        }
     }
 }
