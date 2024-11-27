aba6c239e106e6c6a3b3c0f3405885e9329d8621
Julien Guntermann <julienguntermann@web.de>
2024-11-27 18:43:13 +0100

    refactor: add Country show function with weight history

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Country;
+use Illuminate\Support\Facades\DB;
 
 class CountryController extends Controller
 {
@@ -14,6 +15,39 @@ class CountryController extends Controller
 
         return inertia('Country/CountryIndex', [
             'countries' => $countries,
+        ]);
+    }
+
+    public function show(Country $country)
+    {
+        $countryId = $country->id;
+
+        // TODO Refactor to service
+        $query = "
+            SELECT
+              market_data.date,
+              SUM(market_data.weight) AS weight
+            FROM
+              market_data
+              JOIN companies ON companies.id = market_data.company_id
+            WHERE
+              companies.country_id = ?
+            GROUP BY
+              market_data.date
+            ORDER BY
+              market_data.date
+        ";
+
+        $countryMarketData = DB::select($query, [$countryId]);
+
+        $weightHistory = [
+            'dates' => array_map(fn($data) => $data->date, $countryMarketData),
+            'weights' => array_map(fn($data) => $data->weight, $countryMarketData),
+        ];
+
+        return inertia('Country/CountryShow', [
+            'country' => $country,
+            'weightHistory' => $weightHistory,
         ]);
     }
 }
