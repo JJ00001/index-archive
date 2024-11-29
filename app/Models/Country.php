8e4787457158aceeabc1dae644d7c3ebb8a266ea
Julien Guntermann <julienguntermann@web.de>
2024-11-29 14:36:20 +0100

    feat: apply HasActiveCompany global scope to Sector, Country models

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\HasActiveCompanyScope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+#[ScopedBy(HasActiveCompanyScope::class)]
 class Country extends Model
 {
     protected $fillable = [
