ab915f40a553150a6068e569c4029d0632d6043c
Julien Guntermann <julienguntermann@web.de>
2024-12-08 10:58:06 +0100

    feat: set fixed max height for CompanyIndexTable.vue

--- resources/js/Components/CompanyIndexTable.vue
+++ resources/js/Components/CompanyIndexTable.vue
@@ -29,41 +29,43 @@ const handleSuccess = (response) => {
 </script>
 
 <template>
-    <data-table :value="companyData" @row-click="handleRowClick" selection-mode="single">
-        <column class="w-1/12" field="rank" header="Position"/>
-        <column class="w-7/12" header="Unternehmen">
-            <template #body="{ data }">
-                <div class="flex items-center">
-                    <div class="w-20 h-8 mr-4 flex-shrink-0">
-                        <company-logo :logo-path="data.logo" class="w-full h-full"/>
+    <div class="max-h-[550px] overflow-y-auto">
+        <data-table :value="companyData" selection-mode="single" @row-click="handleRowClick">
+            <column class="w-1/12" field="rank" header="Position"/>
+            <column class="w-7/12" header="Unternehmen">
+                <template #body="{ data }">
+                    <div class="flex items-center">
+                        <div class="w-20 h-8 mr-4 flex-shrink-0">
+                            <company-logo :logo-path="data.logo" class="w-full h-full"/>
+                        </div>
+                        <div>
+                            <span class="font-bold">{{ data.name }}</span><br>
+                            <span class="text-sm text-gray-500">{{ data.ticker }}</span>
+                        </div>
                     </div>
-                    <div>
-                        <span class="font-bold">{{ data.name }}</span><br>
-                        <span class="text-sm text-gray-500">{{ data.ticker }}</span>
-                    </div>
-                </div>
-            </template>
-        </column>
-        <column class="w-2/12" field="latest_weight" header="Gewichtung">
-            <template #body="{ data }">
-                {{ $n(Number(data.latest_weight), 'percent') }}
-            </template>
-        </column>
-        <column class="w-2/12" field="market_capitalization" header="Marktkap.">
-            <template #body="{ data, field}">
-                {{ $n(Number(data[field]), 'currencyUSDCompact') }}
-            </template>
-        </column>
-    </data-table>
-    <WhenVisible :buffer="1000" :params="{
+                </template>
+            </column>
+            <column class="w-2/12" field="latest_weight" header="Gewichtung">
+                <template #body="{ data }">
+                    {{ $n(Number(data.latest_weight), 'percent') }}
+                </template>
+            </column>
+            <column class="w-2/12" field="market_capitalization" header="Marktkap.">
+                <template #body="{ data, field}">
+                    {{ $n(Number(data[field]), 'currencyUSDCompact') }}
+                </template>
+            </column>
+        </data-table>
+        <WhenVisible :buffer="400" :params="{
                     data: {
                         page: nextPage,
                     },
                     preserveUrl: true,
                     onSuccess: handleSuccess,
                 }" always>
-        <template #default>
-            Lädt weitere Unternehmen...
-        </template>
-    </WhenVisible>
+            <template #default>
+                Lädt ...
+            </template>
+        </WhenVisible>
+    </div>
 </template>
