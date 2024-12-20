55033353f80a84c2e910073763fea7629a4e7dc7
Julien Guntermann <julienguntermann@web.de>
2024-12-20 23:58:56 +0100

    refactor: implement mass insertion for performance gain

--- app/Http/Services/HoldingDataService.php
+++ app/Http/Services/HoldingDataService.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Services;
 
-use App\Jobs\FetchCompanyLogo;
 use App\Models\AssetClass;
 use App\Models\Company;
 use App\Models\Country;
@@ -10,9 +9,13 @@ use App\Models\Currency;
 use App\Models\Exchange;
 use App\Models\MarketData;
 use App\Models\Sector;
+use DateMalformedStringException;
 use DateTime;
-use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Log;
+use JsonException;
+use Random\RandomException;
 
 class HoldingDataService
 {
@@ -21,44 +24,118 @@ class HoldingDataService
         $directoryPath = storage_path('holdingsData/');
         $filePath = glob($directoryPath . $filename)[0];
         $fileContent = file_get_contents($filePath);
-        $jsonData = json_decode($fileContent, true);
+        $jsonData = json_decode($fileContent, true, 512, JSON_THROW_ON_ERROR);
         $dateFromFilename = pathinfo(basename($filename), PATHINFO_FILENAME);
 
-        foreach ($jsonData['aaData'] as $data) {
-            $ticker = $data[0];
-            $companyName = $data[1];
-            $sector = $data[2];
-            $assetClass = $data[3];
-            // Raw market capitalization value is in thousands. Multiply by 1000 to convert it to the actual value.
-            $marketCap = $data[4]['raw'] * 1000;
-            // Raw weight value is in percentage. Divide by 100 to convert it decimal representation.
-            $weight = $data[5]['raw'] / 100;
-            $isin = $data[8];
-            $sharePrice = $data[9]['raw'];
-            $country = $data[10];
-            $exchange = $data[11];
-            $currency = $data[12];
+        $companies = [];
+        $marketData = [];
 
-            if ($assetClass === 'Aktien') {
-                // Attributes beside ISIN might change
-                $company = Company::updateOrCreate(['isin' => $isin], [
+        $sectors = Sector::withoutGlobalScopes()->pluck('id', 'name');
+        $countries = Country::withoutGlobalScopes()->pluck('id', 'name');
+        $exchanges = Exchange::pluck('id', 'name');
+        $currencies = Currency::pluck('id', 'name');
+        $assetClasses = AssetClass::pluck('id', 'name');
+
+        foreach ($jsonData['aaData'] as $holding) {
+            $ticker = $holding[0];
+            $companyName = $holding[1];
+            $sectorName = $holding[2];
+            $assetClassName = $holding[3];
+            $marketCapRaw = $holding[4]['raw'] ?? 0;
+            $weightRaw = $holding[5]['raw'] ?? 0;
+            $isin = $holding[8];
+            $sharePriceRaw = $holding[9]['raw'] ?? 0;
+            $countryName = $holding[10];
+            $exchangeName = $holding[11];
+            $currencyName = $holding[12];
+
+            if ($assetClassName === 'Aktien') {
+                if (!isset($sectors[$sectorName])) {
+                    $newSector = Sector::firstOrCreate(['name' => $sectorName]);
+                    $sectors[$sectorName] = $newSector->id;
+                }
+                $sectorId = $sectors[$sectorName];
+
+                if (!isset($countries[$countryName])) {
+                    $newCountry = Country::firstOrCreate(['name' => $countryName]);
+                    $countries[$countryName] = $newCountry->id;
+                }
+                $countryId = $countries[$countryName];
+
+                if (!isset($exchanges[$exchangeName])) {
+                    $newExchange = Exchange::firstOrCreate(['name' => $exchangeName]);
+                    $exchanges[$exchangeName] = $newExchange->id;
+                }
+                $exchangeId = $exchanges[$exchangeName];
+
+                if (!isset($currencies[$currencyName])) {
+                    $newCurrency = Currency::firstOrCreate(['name' => $currencyName]);
+                    $currencies[$currencyName] = $newCurrency->id;
+                }
+                $currencyId = $currencies[$currencyName];
+
+                if (!isset($assetClasses[$assetClassName])) {
+                    $newAssetClass = AssetClass::firstOrCreate(['name' => $assetClassName]);
+                    $assetClasses[$assetClassName] = $newAssetClass->id;
+                }
+                $assetClassId = $assetClasses[$assetClassName];
+
+                $companies[] = [
+                    'isin' => $isin,
                     'ticker' => $ticker,
                     'name' => $companyName,
-                    'sector_id' => Sector::firstOrCreate(['name' => $sector])->id,
-                    'country_id' => Country::firstOrCreate(['name' => $country])->id,
-                    'exchange_id' => Exchange::firstOrCreate(['name' => $exchange])->id,
-                    'currency_id' => Currency::firstOrCreate(['name' => $currency])->id,
-                    'asset_class_id' => AssetClass::firstOrCreate(['name' => $assetClass])->id,
-                ]);
+                    'sector_id' => $sectorId,
+                    'country_id' => $countryId,
+                    'exchange_id' => $exchangeId,
+                    'currency_id' => $currencyId,
+                    'asset_class_id' => $assetClassId,
+                    'updated_at' => now(),
+                    'created_at' => now(),
+                ];
 
-                MarketData::create([
-                    'company_id' => $company->id,
-                    'market_capitalization' => max($marketCap, 0),
-                    'weight' => max($weight, 0),
-                    'share_price' => max($sharePrice, 0),
+                $marketData[] = [
+                    'company_id' => $isin, // temporary reference
+                    'market_capitalization' => max($marketCapRaw * 1000, 0),
+                    'weight' => max($weightRaw / 100, 0),
+                    'share_price' => max($sharePriceRaw, 0),
                     'date' => $dateFromFilename,
-                ]);
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
             }
         }
+
+        Log::info('Upserting companies...');
+
+        Company::withoutGlobalScopes()
+            ->upsert(
+                $companies,
+                ['isin'],
+                ['ticker', 'name', 'sector_id', 'country_id', 'exchange_id', 'currency_id', 'asset_class_id', 'updated_at']
+            );
+
+        Log::info('Companies upserted. Resolving Market Data IDs...');
+
+        $companyIds = Company::withoutGlobalScopes()
+            ->whereIn('isin', array_column($companies, 'isin'))
+            ->pluck('id', 'isin');
+
+        foreach ($marketData as &$data) {
+            $isin = $data['company_id'];
+            if (isset($companyIds[$isin])) {
+                $data['company_id'] = $companyIds[$isin];
+            } else {
+                // If we don't find a matching company, log it or handle as needed
+                Log::warning('No matching company found for ISIN: ' . $isin);
+            }
+        }
+
+        unset($data);
+
+        Log::info('Inserting Market Data...');
+
+        MarketData::insert($marketData);
+
+        Log::info('Data processing complete!');
     }
 }
