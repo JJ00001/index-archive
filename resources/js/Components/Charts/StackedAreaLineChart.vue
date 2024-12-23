79f5a49f672228122dfa3d35341ee9bcc4101b08
Julien Guntermann <julienguntermann@web.de>
2024-12-23 14:58:57 +0100

    feat: add stacked area line chart extending BaseLineChart

---
+++ resources/js/Components/Charts/StackedAreaLineChart.vue
@@ -0,0 +1,22 @@
+<script setup>
+import BaseLineChart from "@/Components/Charts/BaseLineChart.vue";
+
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true
+    }
+});
+
+const modifyOptions = (options) => {
+    options.scales.y.stacked = true;
+    options.scales.y.min = 0;
+    options.scales.y.max = 100;
+
+    return options;
+};
+</script>
+
+<template>
+    <base-line-chart :data="data" :modifyOptions="modifyOptions"/>
+</template>
