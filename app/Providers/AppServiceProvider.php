f007e20799063af15f35d7a777e09b549a42f085
Julien Guntermann <julienguntermann@web.de>
2024-12-23 14:23:13 +0100

    feat: enable debugging for my ip in prod environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (request()->ip() === '104.28.62.24') {
+            config(['app.debug' => true]);
+        }
     }
 }
