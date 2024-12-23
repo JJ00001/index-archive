adf01d9aba6054394f1206a27bc1a2b45a60d523
Julien Guntermann <julienguntermann@web.de>
2024-12-23 13:02:53 +0100

    feat: add redirect from / to company index

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,8 @@ use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\CountryController;
 use App\Http\Controllers\SectorController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Dashboard');
-});
+Route::redirect('/', '/companies');
 
 Route::resource('companies', CompanyController::class)->only(['index', 'show']);
 Route::resource('countries', CountryController::class)->only(['index', 'show']);
