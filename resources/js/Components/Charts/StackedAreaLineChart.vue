638090ae066a73340369f7b21c5057855724928f
Julien Guntermann <julienguntermann@web.de>
2025-01-05 11:12:40 +0100

    feat: add i18n support for tick formatting

--- resources/js/Components/Charts/StackedAreaLineChart.vue
+++ resources/js/Components/Charts/StackedAreaLineChart.vue
@@ -1,5 +1,6 @@
 <script setup>
 import BaseLineChart from "@/Components/Charts/BaseLineChart.vue";
+import { useI18n } from "vue-i18n";
 
 const props = defineProps({
     data: {
@@ -8,9 +9,18 @@ const props = defineProps({
     }
 });
 
+const {n, d} = useI18n();
+
 const modifyOptions = (options) => {
     options.scales.y.stacked = true;
     options.scales.y.min = 0;
+    options.scales.y.ticks.callback = function (value) {
+        return n(value, {style: 'percent', maximumFractionDigits: 0});
+    };
+    options.scales.x.ticks.callback = function (value, index) {
+        // Show every 12th label
+        return index % 12 === 0 ? d(new Date(this.getLabelForValue(value)), 'short') : '';
+    }
 
     return options;
 };
