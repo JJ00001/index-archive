5fed02edaa54313cc015480b1f9f968dda126916
Julien Guntermann <julienguntermann@web.de>
2025-01-27 23:24:39 +0100

    feat: add breadcrumb component

---
+++ resources/js/Components/Breadcrumbs.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Breadcrumb from 'primevue/breadcrumb';
+import { Link } from "@inertiajs/vue3";
+
+const props = defineProps({
+    items: {
+        type: Array,
+        required: true
+    },
+})
+</script>
+
+<template>
+    <Breadcrumb :model="items" class="mb-6 !ps-0">
+        <template #item="{ item }">
+            <Link v-if="item.route" :href="item.route">
+                <span class="hover:underline">{{ item.label }}</span>
+            </Link>
+            <span v-else class="font-thin">{{ item.label }}</span>
+        </template>
+    </Breadcrumb>
+</template>
