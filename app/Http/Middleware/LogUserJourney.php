3902222d30fdbb56cc2bcdcd2ce3c7875a298480
Julien Guntermann <julienguntermann@web.de>
2025-01-30 22:57:44 +0100

    feat: add and register LogUserJourney middleware

---
+++ app/Http/Middleware/LogUserJourney.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogUserJourney
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->isMethod('get')) {
+            $ip = $request->ip();
+            $userAgent = $request->userAgent();
+            $url = $request->fullUrl();
+            $referrer = $request->header('referer', 'Direct Visit');
+            $sessionId = Session::getId();
+
+            $locationResponse = Http::get("https://ip-api.com/json/{$ip}");
+            if ($locationResponse->successful()) {
+                $locationData = $locationResponse->json();
+                $country = $locationData['country'] ?? 'Unknown';
+                $city = $locationData['city'] ?? 'Unknown';
+            } else {
+                $country = $city = 'Unknown';
+            }
+
+            Log::info("User Journey:
+            [IP: $ip]
+            [User-Agent: $userAgent]
+            [Country: $country]
+            [City: $city]
+            [URL: $url]
+            [Referrer: $referrer]
+            [Session: $sessionId]"
+            );
+        }
+
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\LogUserJourney;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+        $middleware->append(LogUserJourney::class);
 
         //
     })
