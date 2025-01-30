75692d133e5e3d35838d1e5220be25a7ad3c75b3
Julien Guntermann <julienguntermann@web.de>
2025-01-30 19:48:49 +0100

    feat: bypass adblockers by proxying analytics request

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\CountryController;
 use App\Http\Controllers\SectorController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/companies');
@@ -10,3 +12,14 @@ Route::redirect('/', '/companies');
 Route::resource('companies', CompanyController::class)->only(['index', 'show']);
 Route::resource('countries', CountryController::class)->only(['index', 'show']);
 Route::resource('sectors', SectorController::class)->only(['index', 'show']);
+
+Route::get('/analytics.js', function () {
+    return response()->file(public_path('js/plausible.js'), [
+        'Content-Type' => 'application/javascript'
+    ]);
+});
+
+Route::post('/analytics-event', function (Request $request) {
+    Log::info('analytics.js');
+    return Http::post('https://plausible.io/api/event', $request->all());
+});
