3d0f14746d3d3170d6469d0060b2cc812b7b1e01
Julien Guntermann <julienguntermann@web.de>
2025-01-30 19:10:40 +0100

    feat: add analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Analytics -->
+        <script defer data-domain="msci-world-tracker.com" src="https://plausible.io/js/script.js"></script>
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
