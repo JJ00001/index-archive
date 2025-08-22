4fd943fb2e6923ead69e783bd8e8931e15de289b
Julien Guntermann <julienguntermann@web.de>
2025-08-22 17:24:16 +0200

    feat: add shadcn card component

---
+++ resources/js/Components/ui/card/CardAction.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { cn } from '@/lib/utils'
+
+const props = defineProps({
+    class: { type: null, required: false },
+})
+</script>
+
+<template>
+    <div
+        :class="
+      cn(
+        'col-start-2 row-span-2 row-start-1 self-start justify-self-end',
+        props.class,
+      )
+    "
+        data-slot="card-action"
+    >
+        <slot />
+    </div>
+</template>
---
+++ resources/js/Components/ui/card/CardDescription.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { cn } from '@/lib/utils'
+
+const props = defineProps({
+    class: { type: null, required: false },
+})
+</script>
+
+<template>
+    <p
+        :class="cn('text-muted-foreground text-sm', props.class)"
+        data-slot="card-description"
+    >
+        <slot />
+    </p>
+</template>
