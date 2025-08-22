980308b26ba7167d9947e7f7b1e3caa3cfa65750
Julien Guntermann <julienguntermann@web.de>
2025-08-22 18:19:21 +0200

    feat: replace PrimeVue Breadcrumb with shadcn Breadcrumb

--- resources/js/Components/Breadcrumbs.vue
+++ resources/js/Components/Breadcrumbs.vue
@@ -1,23 +1,36 @@
 <script setup>
-import Breadcrumb from 'primevue/breadcrumb'
 import { Link } from '@inertiajs/vue3'
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from '@/Components/ui/breadcrumb'
 
 const props = defineProps({
     items: {
         type: Array,
-        required: true
+        required: true,
     },
 })
 </script>
 
 <template>
-  <Breadcrumb :model="items"
-              class="mb-6 ps-0!">
-        <template #item="{ item }">
-            <Link v-if="item.route" :href="item.route">
-                <span class="hover:underline">{{ item.label }}</span>
-            </Link>
-            <span v-else class="font-thin">{{ item.label }}</span>
-        </template>
+    <Breadcrumb class="mb-6">
+        <BreadcrumbList>
+            <template v-for="(item, index) in items"
+                      :key="index">
+                <BreadcrumbItem>
+                    <BreadcrumbLink v-if="item.route"
+                                    as-child>
+                        <Link :href="item.route">{{ item.label }}</Link>
+                    </BreadcrumbLink>
+                    <BreadcrumbPage v-else>{{ item.label }}</BreadcrumbPage>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator v-if="index < items.length - 1" />
+            </template>
+        </BreadcrumbList>
     </Breadcrumb>
 </template>
