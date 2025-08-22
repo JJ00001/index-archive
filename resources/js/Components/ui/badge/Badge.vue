fdbf8ae7607ffdf8c3e2fb5a4514d6ec3d664c0c
Julien Guntermann <julienguntermann@web.de>
2025-08-22 18:23:44 +0200

    feat: add shadcn badge component

---
+++ resources/js/Components/ui/badge/Badge.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactiveOmit } from '@vueuse/core'
+import { Primitive } from 'reka-ui'
+import { cn } from '@/lib/utils'
+import { badgeVariants } from '.'
+
+const props = defineProps({
+    asChild: { type: Boolean, required: false },
+    as: { type: null, required: false },
+    variant: { type: null, required: false },
+    class: { type: null, required: false },
+})
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+    <Primitive
+        :class="cn(badgeVariants({ variant }), props.class)"
+        data-slot="badge"
+        v-bind="delegatedProps"
+    >
+        <slot />
+    </Primitive>
+</template>
