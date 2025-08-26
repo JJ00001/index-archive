f423893a850627961a515cbf321f1f056847c57a
Julien Guntermann <julienguntermann@web.de>
2025-08-26 20:43:44 +0200

    feat: remove StackedLineChart.vue component and usages

--- resources/js/Pages/Company/CompanyIndex.vue
+++ resources/js/Pages/Company/CompanyIndex.vue
@@ -1,9 +1,7 @@
 <script setup>
 import LayoutMain from '@/Layouts/LayoutMain.vue'
 import CompanyIndexTable from '@/Components/CompanyIndexTable.vue'
-import { Card, CardContent, CardHeader, CardTitle } from '@/Components/ui/card'
-import StackedLineChart from '@/Components/Charts/StackedLineChart.vue'
-import { ref } from 'vue'
+import { Card, CardContent } from '@/Components/ui/card'
 
 const props = defineProps({
     companies: {
@@ -14,35 +12,16 @@ const props = defineProps({
         type: Number,
         required: true
     },
-    multipleWeightHistory: {
-        type: Object,
-        required: true
-    },
-});
-
-const multipleWeightHistoryData = ref({
-    labels: props.multipleWeightHistory.labels,
-    datasets: props.multipleWeightHistory.datasets
 });
 </script>
 
 <template>
     <layout-main>
-        <div class="space-y-10">
-            <Card>
-                <CardContent>
-                    <company-index-table :companies="companies" :next-page="nextPage"/>
-                </CardContent>
-            </Card>
-            <Card>
-                <CardHeader>
-                    <CardTitle class="text-2xl font-bold">Gewichtungs-Vergleich (Top 10)</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <stacked-line-chart :data="multipleWeightHistoryData"
-                                      class="h-160" />
-                </CardContent>
-            </Card>
-        </div>
+        <Card>
+            <CardContent>
+                <company-index-table :companies="companies"
+                                     :next-page="nextPage" />
+            </CardContent>
+        </Card>
     </layout-main>
 </template>
--- resources/js/Pages/Sector/SectorIndex.vue
+++ resources/js/Pages/Sector/SectorIndex.vue
@@ -1,46 +1,25 @@
 <script setup>
 import LayoutMain from '@/Layouts/LayoutMain.vue'
-import { Card, CardContent, CardHeader, CardTitle } from '@/Components/ui/card'
+import { Card, CardContent } from '@/Components/ui/card'
 import SectorIndexTable from '@/Components/SectorIndexTable.vue'
 import { ref } from 'vue'
-import StackedLineChart from '@/Components/Charts/StackedLineChart.vue'
 
 const props = defineProps({
     sectors: {
         type: Object,
         required: true
     },
-    multipleWeightHistory: {
-        type: Object,
-        required: true
-    },
 })
 
 const sectorData = ref([...props.sectors]);
-
-const multipleWeightHistoryData = ref({
-    labels: props.multipleWeightHistory.labels,
-    datasets: props.multipleWeightHistory.datasets
-});
 </script>
 
 <template>
     <layout-main>
-        <div class="space-y-10">
-            <Card>
-                <CardContent>
-                    <sector-index-table :sector-data="sectorData"/>
-                </CardContent>
-            </Card>
-            <Card>
-                <CardHeader>
-                    <CardTitle class="text-2xl font-bold">Gewichtungs-Vergleich</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <stacked-line-chart :data="multipleWeightHistoryData"
-                                      class="h-160" />
-                </CardContent>
-            </Card>
-        </div>
+        <Card>
+            <CardContent>
+                <sector-index-table :sector-data="sectorData" />
+            </CardContent>
+        </Card>
     </layout-main>
 </template>
