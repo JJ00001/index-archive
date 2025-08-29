203649cf9919f9200f45e2c6eb39e8aaa9c7a094
Julien Guntermann <julienguntermann@web.de>
2025-08-29 20:10:43 +0200

    feat: add ScrapeHoldingDataJob to handle index data scraping operations

---
+++ app/Jobs/ScrapeHoldingDataJob.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Http\Services\HoldingDataService;
+use App\Models\Index;
+use DateMalformedStringException;
+use DateTime;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
+use JsonException;
+use Random\RandomException;
+
+class ScrapeHoldingDataJob implements ShouldQueue
+{
+
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public Index $index,
+        public DateTime $startDate,
+        public DateTime $endDate
+    ) {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @throws DateMalformedStringException
+     * @throws GuzzleException
+     * @throws RandomException
+     * @throws JsonException
+     */
+    public function handle(HoldingDataService $holdingDataService): void
+    {
+        Log::info('Starting scrape job for index: '.$this->index->name);
+
+        $holdingDataService->scrape($this->index, $this->startDate, $this->endDate);
+
+        Log::info('Scrape job completed for index: '.$this->index->name);
+    }
+
+}
