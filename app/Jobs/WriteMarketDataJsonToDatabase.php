e94649f7cf5373e2a0ebe572633d3f197cc11946
Julien Guntermann <julienguntermann@web.de>
2025-08-30 19:32:45 +0200

    feat: add WriteMarketDataJsonToDatabase job

---
+++ app/Jobs/WriteMarketDataJsonToDatabase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Http\Services\HoldingDataService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
+use JsonException;
+
+class WriteMarketDataJsonToDatabase implements ShouldQueue
+{
+
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public string $fullFilePath
+    ) {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @throws JsonException
+     */
+    public function handle(): void
+    {
+        Log::info('Starting write market data job for file: '.$this->fullFilePath);
+
+        $service = new HoldingDataService();
+        $service->writeHoldingDataToDB($this->fullFilePath);
+
+        Log::info('Write market data job completed for file: '.$this->fullFilePath);
+    }
+
+}
