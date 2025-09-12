264704a162404504860f55ef3bd543f1e2e91b82
Julien Guntermann <julienguntermann@web.de>
2025-09-12 12:28:03 +0200

    feat: remove LogUserJourney middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
-use App\Http\Middleware\LogUserJourney;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,7 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
-        $middleware->append(LogUserJourney::class);
 
         //
     })
