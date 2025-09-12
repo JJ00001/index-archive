a8cd9cf58dd2280d70d0b385f204ec0a3cd42a65
Julien Guntermann <julienguntermann@web.de>
2025-09-12 10:46:45 +0200

    feat: reduce StatCardGroup spacing and padding

--- resources/js/Components/StatCardGroup.vue
+++ resources/js/Components/StatCardGroup.vue
@@ -14,11 +14,11 @@ defineProps({
     <div class="flex space-x-5">
         <Card v-for="stat in stats"
               :key="stat.title"
-              class="flex-1">
-            <CardHeader>
+              class="flex-1 gap-4 py-4">
+            <CardHeader class="px-4">
                 <CardTitle class="whitespace-nowrap">{{ stat.title }}</CardTitle>
             </CardHeader>
-            <CardContent>
+            <CardContent class="px-4">
                 <Link v-if="stat.route" :href="stat.route" class="underline underline-offset-2">{{ stat.value }}</Link>
                 <span v-else>{{ stat.value }}</span>
             </CardContent>
