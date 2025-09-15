682adb174964e9a47de48186b187643222ad0f37
Julien Guntermann <julienguntermann@web.de>
2025-09-15 20:18:47 +0200

    feat: display finer weight

--- app/Models/MarketData.php
+++ app/Models/MarketData.php
@@ -25,10 +25,9 @@ class MarketData extends Model
         return $this->belongsTo(IndexHolding::class);
     }
 
-    // TODO remove and refactor raw SQL statements to use eloquent
     protected function weight(): Attribute
     {
-        return Attribute::make(fn(float $value) => round($value, 3));
+        return Attribute::make(fn(float $value) => round($value, 5));
     }
 
     public static function maxDate(): mixed
