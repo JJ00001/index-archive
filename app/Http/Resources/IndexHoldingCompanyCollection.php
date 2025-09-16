616e2002d031e2d27e84f5b07aceaec91b02585f
Julien Guntermann <julienguntermann@web.de>
2025-09-16 19:08:07 +0200

    feat: add IndexHoldingCompanyCollection resource with rank and weight attributes

---
+++ app/Http/Resources/IndexHoldingCompanyCollection.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\IndexHolding;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class IndexHoldingCompanyCollection extends ResourceCollection
+{
+
+    public function toArray(Request $request): array
+    {
+        $rankedCompanies = $this->collection
+            ->map(fn(IndexHolding $holding) => [
+                'company' => $holding->company,
+                'weight' => $this->getCurrentWeight($holding),
+            ])
+            ->sortByDesc('weight')
+            ->values()
+            ->map(fn($item, $index) => [
+                'id' => $item['company']->id,
+                'name' => $item['company']->name,
+                'ticker' => $item['company']->ticker,
+                'logo' => $item['company']->logo,
+                'weight' => $item['weight'],
+                'rank' => $index + 1,
+            ]);
+
+        return $rankedCompanies->toArray();
+    }
+
+    private function getCurrentWeight($indexHolding): float
+    {
+        return $indexHolding->marketData
+                   ->sortByDesc('date')
+                   ->first()?->weight ?? 0.0;
+    }
+
+}
