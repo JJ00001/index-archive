41dcc38725ada572f349f66fb0d14a036b8e71ac
Julien Guntermann <julienguntermann@web.de>
2025-10-16 18:47:42 +0200

    feat: add index-specific country stats scopes

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -29,4 +29,35 @@ class Country extends Model
     {
         $query->withCompaniesCount();
     }
+
+    public function scopeWithWeightInIndex(Builder $query, Index $index): void
+    {
+        $query->addSelect([
+            'weight' => $index->latestMarketData()
+                              ->whereColumn('index_holdings.company_id', 'companies.id')
+                              ->whereColumn('companies.country_id', 'countries.id')
+                              ->selectRaw('SUM(market_data.weight)'),
+        ]);
+    }
+
+    public function scopeWithCompaniesCountInIndex(Builder $query, Index $index): void
+    {
+        $query->addSelect([
+            'companies_count' => Company::query()
+                                        ->whereColumn('country_id', 'countries.id')
+                                        ->whereExists(function ($subQuery) use ($index) {
+                                            $subQuery->from('index_holdings')
+                                                     ->whereColumn('index_holdings.company_id', 'companies.id')
+                                                     ->where('index_holdings.index_id', $index->id);
+                                        })
+                                        ->selectRaw('COUNT(*)'),
+        ]);
+    }
+
+    public function scopeWithStatsInIndex(Builder $query, Index $index): void
+    {
+        $query
+            ->withWeightInIndex($index)
+            ->withCompaniesCountInIndex($index);
+    }
 }
