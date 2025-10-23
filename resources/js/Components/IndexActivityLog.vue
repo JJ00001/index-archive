0e076cac1d81aa83e598334b975f978f5fb04afd
Julien Guntermann <julienguntermann@web.de>
2025-10-23 12:34:42 +0200

    feat: add IndexActivityLog component

---
+++ resources/js/Components/IndexActivityLog.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { Badge } from '@/Components/ui/badge'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/Components/ui/table'
+import CompanyDisplay from '@/Components/CompanyDisplay.vue'
+import { useI18n } from 'vue-i18n'
+
+const props = defineProps({
+  activities: {
+    type: Array,
+    required: true,
+  },
+})
+
+const { t } = useI18n()
+
+const getActivityBadgeVariant = (description) => {
+  return description === 'company_added_to_index' ? 'default' : 'destructive'
+}
+
+const getActivityLabel = (description) => {
+  return t(`activity.${description}`)
+}
+</script>
+
+<template>
+  <div v-if="activities.length === 0"
+       class="text-sm text-muted-foreground">
+    {{ $t('activity.noRecentActivity') }}
+  </div>
+  <div v-else
+       class="h-[550px] overflow-auto">
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead class="w-2/12">{{ $t('activity.action') }}</TableHead>
+          <TableHead class="w-8/12">{{ $t('company.name') }}</TableHead>
+          <TableHead class="w-2/12">{{ $t('activity.dataDate') }}</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow
+            v-for="activity in activities"
+            :key="activity.id"
+        >
+          <TableCell>
+            <Badge :variant="getActivityBadgeVariant(activity.description)">
+              {{ getActivityLabel(activity.description) }}
+            </Badge>
+          </TableCell>
+          <TableCell>
+            <CompanyDisplay :company="activity.company" />
+          </TableCell>
+          <TableCell class="text-sm text-muted-foreground">
+            {{ activity.date }}
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </div>
+</template>
