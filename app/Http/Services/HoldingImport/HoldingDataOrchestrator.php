5abff96b92ccd32a8e54379c927eee090a104561
Julien Guntermann <julienguntermann@web.de>
2025-10-23 13:48:44 +0200

    feat: integrate change detection and activity logging into HoldingDataOrchestrator

--- app/Http/Services/HoldingImport/HoldingDataOrchestrator.php
+++ app/Http/Services/HoldingImport/HoldingDataOrchestrator.php
@@ -12,6 +12,8 @@ class HoldingDataOrchestrator
         private CompanyUpsertService $companyUpsert,
         private IndexHoldingService $indexHoldingService,
         private MarketDataService $marketDataService,
+        private IndexHoldingChangeDetector $changeDetector,
+        private IndexHoldingActivityLogger $activityLogger,
     ) {}
 
     /**
@@ -20,15 +22,19 @@ class HoldingDataOrchestrator
     public function processHoldingFile(string $fullFilePath): void
     {
         $index = $this->extractIndexFromPath($fullFilePath);
+        $date = $this->extractDateFromPath($fullFilePath);
+
         [$companiesFromFile, $marketDataFromFile] = $this->parser->parse($index, $fullFilePath);
 
         $this->companyUpsert->upsert($companiesFromFile);
+
+        $changes = $this->changeDetector->detectChanges($index, $companiesFromFile);
 
         $this->indexHoldingService->createForIndex($index, $companiesFromFile);
 
         $this->marketDataService->insert($index, $marketDataFromFile);
 
-        $this->marketDataService->insert($index, $marketData);
+        $this->logChanges($index, $changes, $date);
     }
 
     private function extractIndexFromPath(string $fullFilePath): Index
@@ -37,5 +43,21 @@ class HoldingDataOrchestrator
         $indexId = $pathParts[array_search('holdingsData', $pathParts) + 1];
 
         return Index::findOrFail($indexId);
+    }
+
+    private function extractDateFromPath(string $fullFilePath): string
+    {
+        return pathinfo(basename($fullFilePath), PATHINFO_FILENAME);
+    }
+
+    private function logChanges(Index $index, array $changes, string $date): void
+    {
+        if ($changes['additions']->isNotEmpty()) {
+            $this->activityLogger->logAdditions($index, $changes['additions'], $date);
+        }
+
+        if ($changes['removals']->isNotEmpty()) {
+            $this->activityLogger->logRemovals($index, $changes['removals'], $date);
+        }
     }
 }
