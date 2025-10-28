fe28772fb7f454f441aedd1182d0a853c974f07d
Julien Guntermann <julienguntermann@web.de>
2025-10-28 20:25:33 +0100

    feat: add `Dialog` components to UI library

---
+++ resources/js/Components/ui/dialog/DialogDescription.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactiveOmit } from '@vueuse/core'
+import { DialogDescription, useForwardProps } from 'reka-ui'
+import { cn } from '@/lib/utils'
+
+const props = defineProps({
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+})
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <DialogDescription
+      :class="cn('text-sm text-muted-foreground', props.class)"
+      v-bind="forwardedProps"
+  >
+    <slot />
+  </DialogDescription>
+</template>
