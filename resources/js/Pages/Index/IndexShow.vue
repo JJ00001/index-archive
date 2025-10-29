8b46109bb20466697408cf12a492e2102af2f974
Julien Guntermann <julienguntermann@web.de>
2025-10-29 17:20:54 +0100

    feat: integrate IndexHolding show dialog in IndexShow view

--- resources/js/Pages/Index/IndexShow.vue
+++ resources/js/Pages/Index/IndexShow.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import LayoutMain from '@/Layouts/LayoutMain.vue'
 import StatCardGroup from '@/Components/StatCardGroup.vue'
 import Breadcrumbs from '@/Components/Breadcrumbs.vue'
@@ -7,7 +8,9 @@ import CompanyIndexTable from '@/Components/CompanyIndexTable.vue'
 import SectorIndexTable from '@/Components/SectorIndexTable.vue'
 import CountryIndexTable from '@/Components/CountryIndexTable.vue'
 import IndexActivityLog from '@/Components/IndexActivityLog.vue'
+import HoldingDataShow from '@/Pages/HoldingData/HoldingDataShow.vue'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/Components/ui/card'
+import { Dialog, DialogContent } from '@/Components/ui/dialog'
 
 const props = defineProps({
     index: {
@@ -26,13 +29,13 @@ const props = defineProps({
         type: Object,
         required: true,
     },
-  countries: {
-    type: Object,
-    required: true,
-  },
-  activities: {
-    type: Object,
-    required: true,
+    countries: {
+        type: Object,
+        required: true,
+    },
+    activities: {
+        type: Object,
+        required: true,
     },
 })
 
@@ -50,6 +53,24 @@ const breadcrumbItems = [
 ]
 
 const companiesCount = props.companies.data.length
+
+const showIndexHoldingDialog = ref(false)
+const selectedIndexHolding = ref(null)
+const isLoading = ref(false)
+
+const handleIndexHoldingRowClick = async (company) => {
+    isLoading.value = true
+    try {
+        const response = await fetch(route('api.index-holdings.show', { indexHolding: company.index_holding_id }))
+        const data = await response.json()
+        selectedIndexHolding.value = data.props.indexHolding
+        showIndexHoldingDialog.value = true
+    } catch (error) {
+        console.error('Error fetching IndexHolding details:', error)
+    } finally {
+        isLoading.value = false
+    }
+}
 </script>
 
 <template>
@@ -67,7 +88,8 @@ const companiesCount = props.companies.data.length
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <company-index-table :companies="companies" />
+                    <company-index-table :companies="companies"
+                                         :on-row-click="handleIndexHoldingRowClick" />
                 </CardContent>
             </Card>
             <Card>
@@ -81,28 +103,35 @@ const companiesCount = props.companies.data.length
                     <sector-index-table :sector-data="sectors.data" />
                 </CardContent>
             </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>{{ t('country.name', 2) }}</CardTitle>
-              <CardDescription>
-                Country allocation for the {{ index.name }} index
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <country-index-table :country-data="countries.data" />
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Recent Activity</CardTitle>
-              <CardDescription>
-                Companies added to or removed from the {{ index.name }} index
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <index-activity-log :activities="activities" />
+            <Card>
+                <CardHeader>
+                    <CardTitle>{{ t('country.name', 2) }}</CardTitle>
+                    <CardDescription>
+                        Country allocation for the {{ index.name }} index
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <country-index-table :country-data="countries.data" />
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Recent Activity</CardTitle>
+                    <CardDescription>
+                        Companies added to or removed from the {{ index.name }} index
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <index-activity-log :activities="activities" />
                 </CardContent>
             </Card>
         </div>
+        <Dialog :open="showIndexHoldingDialog"
+                @update:open="showIndexHoldingDialog = $event">
+            <DialogContent class="max-w-5xl max-h-[80vh] overflow-y-auto">
+                <HoldingDataShow v-if="selectedIndexHolding"
+                                 :indexHolding="selectedIndexHolding" />
+            </DialogContent>
+        </Dialog>
     </layout-main>
 </template>
