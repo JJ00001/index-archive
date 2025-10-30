99f7b82e8951721910eb6c1e33a711a128896b5f
Julien Guntermann <julienguntermann@web.de>
2025-10-30 12:00:57 +0100

    feat: add api show endpoints for Country and Sector models

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
 use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\CountryController;
 use App\Http\Controllers\Api\IndexController;
 use App\Http\Controllers\Api\IndexHoldingController;
+use App\Http\Controllers\Api\SectorController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('api.')->group(function () {
     Route::get('/indices/top', [IndexController::class, 'top'])->name('indices.top');
     Route::get('/index-holdings/{indexHolding}', [IndexHoldingController::class, 'show'])->name('index-holdings.show');
+    Route::get('/indices/{index}/countries/{country}', [CountryController::class, 'show'])
+         ->name('indices.countries.show');
+    Route::get('/indices/{index}/sectors/{sector}', [SectorController::class, 'show'])->name('indices.sectors.show');
     Route::get('/companies/top', [CompanyController::class, 'top'])->name('companies.top');
 });
