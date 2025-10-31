785858f18d512426a5331bfd8ec6cd8eb28064aa
Julien Guntermann <julienguntermann@web.de>
2025-10-31 13:26:35 +0100

    fix: remove unused Country and Sector routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,10 @@
 <?php
 
 use App\Http\Controllers\CompanyController;
-use App\Http\Controllers\CountryController;
 use App\Http\Controllers\IndexController;
-use App\Http\Controllers\SectorController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/indices');
 
 Route::resource('companies', CompanyController::class)->only(['index', 'show']);
-Route::resource('countries', CountryController::class)->only(['show']);
-Route::resource('sectors', SectorController::class)->only(['show']);
 Route::resource('indices', IndexController::class)->only(['index', 'show']);
