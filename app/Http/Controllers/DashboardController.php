754e7f8b3667bd472f7f6aaf92ddebaa3873391a
Julien Guntermann <julienguntermann@web.de>
2025-11-03 18:53:50 +0100

    feat: add IndexIndexTable component and pass indices from DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Index;
 use Inertia\Response;
 
 class DashboardController extends Controller
 {
-
     public function index(): Response
     {
-        return inertia('Dashboard');
-    }
+        $indices = Index::withCount('indexHoldings')
+                        ->with('indexProvider:id,name')
+                        ->get();
 
+        return inertia('Dashboard', [
+            'indices' => $indices,
+        ]);
+    }
 }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,10 +1,19 @@
 <script setup>
 import DashboardHero from '@/Components/DashboardHero.vue'
 import LayoutMain from '@/Layouts/LayoutMain.vue'
+import IndexIndexTable from '@/Components/IndexIndexTable.vue'
+
+const props = defineProps({
+    indices: {
+        type: Object,
+        required: true,
+    },
+})
 </script>
 
 <template>
     <layout-main>
         <DashboardHero />
+        <index-index-table :indices="indices" />
     </layout-main>
 </template>
