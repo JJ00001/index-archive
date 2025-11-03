d1685a3052c8134e91385511ac2a79f71ff02fc6
Julien Guntermann <julienguntermann@web.de>
2025-11-03 18:40:16 +0100

    feat: add DashboardHero component

---
+++ resources/js/Components/DashboardHero.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+    <section class="space-y-12 my-20">
+        <h1 class="text-5xl font-semibold tracking-tight text-slate-900 sm:text-6xl md:text-7xl">
+            Explore how global markets evolve
+        </h1>
+        <p class="text-lg leading-relaxed text-slate-600 sm:text-xl">
+            Index Archive shows how the composition of major market indices changes over time.
+            <br>
+            Explore shifts across countries, sectors, and companies to understand what’s really driving the market.
+        </p>
+    </section>
+</template>
