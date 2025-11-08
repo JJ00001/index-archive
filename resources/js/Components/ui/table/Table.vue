4b83ab5399dbc28f017dbd772fe08ac99dd0fd72
Julien Guntermann <julienguntermann@web.de>
2025-11-08 15:56:45 +0100

    feat: add sticky header support

--- resources/js/Components/ui/table/Table.vue
+++ resources/js/Components/ui/table/Table.vue
@@ -3,14 +3,21 @@ import { cn } from '@/lib/utils'
 
 const props = defineProps({
     class: { type: null, required: false },
+    stickyHeader: { type: Boolean, default: true },
 })
+
+const containerStyle = props.stickyHeader ? 'relative w-full' : 'relative w-full overflow-auto'
+
+const tableStyle = props.stickyHeader
+    ? 'w-full caption-bottom text-sm [&_th]:sticky [&_th]:top-0 [&_th]:z-10 [&_th]:bg-background'
+    : 'w-full caption-bottom text-sm'
 </script>
 
 <template>
-    <div class="relative w-full overflow-auto"
+    <div :class="cn(containerStyle)"
          data-slot="table-container">
         <table
-            :class="cn('w-full caption-bottom text-sm', props.class)"
+            :class="cn(tableStyle, props.class)"
             data-slot="table"
         >
             <slot />
