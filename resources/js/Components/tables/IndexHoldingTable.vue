dc90f47ced121e75bca7d460229cdb68a10d7696
Julien Guntermann <julienguntermann@web.de>
2025-11-14 15:22:53 +0100

    feat: add sorting support to IHTable

--- resources/js/Components/tables/IndexHoldingTable.vue
+++ resources/js/Components/tables/IndexHoldingTable.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { createColumnHelper } from '@tanstack/vue-table'
-import { h } from 'vue'
+import { computed, h } from 'vue'
 import { useI18n } from 'vue-i18n'
 import CompanyDisplay from '@/Components/CompanyDisplay.vue'
 import DataTable from '@/Components/ui/data-table/DataTable.vue'
+import DataTableColumnHeader from '@/Components/ui/data-table/DataTableColumnHeader.vue'
+import useDataTableSorting from '@/composables/useDataTableSorting'
 
 const props = defineProps({
     companies: {
@@ -14,15 +16,31 @@ const props = defineProps({
         type: Function,
         default: null,
     },
+    // Sorting
+    sort: {
+        type: Object,
+        required: true,
+    },
+    onRequestSort: {
+        type: Function,
+        required: true,
+    },
 })
 
 const { t, n } = useI18n()
 const columnHelper = createColumnHelper()
 
+const sort = computed(() => props.sort)
+const { sorting, handleSortingChange } = useDataTableSorting(sort, props.onRequestSort)
+
 const columns = [
     columnHelper.accessor('weight', {
         id: 'weight',
-        header: () => t('weight'),
+        header: ({ column }) =>
+            h(DataTableColumnHeader, {
+                column,
+                title: t('weight'),
+            }),
         cell: ({ getValue }) => n(getValue(), 'percentFine'),
         meta: {
             headerClass: 'w-1/12 text-center',
@@ -31,7 +49,11 @@ const columns = [
     }),
     columnHelper.accessor(row => row.company.name, {
         id: 'company',
-        header: () => t('company.name'),
+        header: ({ column }) =>
+            h(DataTableColumnHeader, {
+                column,
+                title: t('company.name'),
+            }),
         cell: ({ row }) => h(CompanyDisplay, { company: row.original.company }),
         meta: {
             headerClass: 'pl-10 w-11/12',
@@ -50,6 +72,8 @@ const handleRowClick = (holding) => {
         :columns="columns"
         :data="companies"
         :on-row-click="handleRowClick"
+        :on-sorting-change="handleSortingChange"
+        :sorting="sorting"
         body-id="index-holding-table-body"
         infinite-scroll-key="companies"
         remember-scroll-key="IndexHoldingTable"
