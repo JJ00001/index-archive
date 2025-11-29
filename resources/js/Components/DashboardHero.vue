bae2bcb5c9ad4349c0002217e30b1f0258fee015
Julien Guntermann <julienguntermann@web.de>
2025-11-29 17:09:18 +0100

    style: update font

--- resources/js/Components/DashboardHero.vue
+++ resources/js/Components/DashboardHero.vue
@@ -3,11 +3,10 @@
 
 <template>
     <section class="mx-auto my-20 max-w-3xl space-y-12 text-center">
-        <h1 class="text-5xl font-semibold tracking-tight text-slate-900 sm:text-6xl md:text-7xl">
+        <h1 class="font-apple-garamond text-5xl tracking-tight text-slate-900 sm:text-6xl md:text-7xl">
             Explore how global markets evolve
         </h1>
-        <p class="text-lg leading-relaxed text-slate-600 sm:text-xl">
-            Index Archive shows how the composition of major market indices changes over time.
+        <h2 class="font-apple-garamond text-2xl leading-relaxed text-slate-600">
             <br>
             Explore shifts across countries, sectors, and companies to understand what’s really driving the market.
         </p>
