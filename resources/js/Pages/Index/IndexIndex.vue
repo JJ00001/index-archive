13fa589ec3264492d78ba4e2c1574f1bcafeffb3
Julien Guntermann <julienguntermann@web.de>
2025-12-09 17:53:16 +0100

    feat: add Index typing to IndexIndex.vue

--- resources/js/Pages/Index/IndexIndex.vue
+++ resources/js/Pages/Index/IndexIndex.vue
@@ -1,16 +1,15 @@
-<script setup>
-import { Head } from '@inertiajs/vue3'
-import { useI18n } from 'vue-i18n'
+<script lang="ts"
+        setup>
+import {Head} from '@inertiajs/vue3'
+import {useI18n} from 'vue-i18n'
 import LayoutMain from '@/Layouts/LayoutMain.vue'
 import IndexIndexTable from '@/Components/tables/IndexIndexTable.vue'
-import { Card, CardContent } from '@/Components/ui/card'
+import {Card, CardContent} from '@/Components/ui/card'
+import type {Index} from "@/interfaces";
 
-const props = defineProps({
-    indices: {
-        type: Object,
-        required: true,
-    },
-})
+defineProps<{
+    indices: Index[]
+}>()
 
 const { t } = useI18n()
 </script>
