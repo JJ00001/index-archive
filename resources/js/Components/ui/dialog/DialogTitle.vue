7e30bae96d8de71b4f75e76312d043f18de988a7
Julien Guntermann <julienguntermann@web.de>
2026-03-22 17:39:52 +0100

    chore: overwrite component as ts

--- resources/js/Components/ui/dialog/DialogTitle.vue
+++ resources/js/Components/ui/dialog/DialogTitle.vue
@@ -1,25 +1,27 @@
-<script setup>
-import { reactiveOmit } from '@vueuse/core'
-import { DialogTitle, useForwardProps } from 'reka-ui'
-import { cn } from '@/lib/utils'
+<script lang="ts"
+        setup>
+import type {DialogTitleProps} from "reka-ui"
+import {DialogTitle, useForwardProps} from "reka-ui"
+import type {HTMLAttributes} from "vue"
+import {reactiveOmit} from "@vueuse/core"
+import {cn} from "@/lib/utils"
 
-const props = defineProps({
-  asChild: { type: Boolean, required: false },
-  as: { type: null, required: false },
-  class: { type: null, required: false },
-})
+const props = defineProps<DialogTitleProps & { class?: HTMLAttributes["class"] }>()
 
-const delegatedProps = reactiveOmit(props, 'class')
+const delegatedProps = reactiveOmit(props, "class")
 
 const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
 <template>
   <DialogTitle
-      :class="
-      cn('text-lg font-semibold leading-none tracking-tight', props.class)
-    "
       v-bind="forwardedProps"
+      :class="
+      cn(
+        'text-lg font-semibold leading-none tracking-tight',
+        props.class,
+      )
+    "
   >
     <slot />
   </DialogTitle>
