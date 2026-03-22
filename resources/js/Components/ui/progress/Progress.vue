17ddcde9661cac3b43fd962906b76f65a32f8e0f
Julien Guntermann <julienguntermann@web.de>
2026-03-22 17:44:16 +0100

    chore: overwrite component as ts

--- resources/js/Components/ui/progress/Progress.vue
+++ resources/js/Components/ui/progress/Progress.vue
@@ -1,36 +1,34 @@
-<script setup>
-import { reactiveOmit } from '@vueuse/core'
-import { ProgressIndicator, ProgressRoot } from 'reka-ui'
-import { cn } from '@/lib/utils'
+<script lang="ts"
+        setup>
+import type {ProgressRootProps} from "reka-ui"
+import {ProgressIndicator, ProgressRoot,} from "reka-ui"
+import type {HTMLAttributes} from "vue"
+import {reactiveOmit} from "@vueuse/core"
+import {cn} from "@/lib/utils"
 
-const props = defineProps({
-    modelValue: { type: [Number, null], required: false, default: 0 },
-    max: { type: Number, required: false },
-    getValueLabel: { type: Function, required: false },
-    getValueText: { type: Function, required: false },
-    asChild: { type: Boolean, required: false },
-    as: { type: null, required: false },
-    class: { type: null, required: false },
-})
+const props = withDefaults(
+    defineProps<ProgressRootProps & { class?: HTMLAttributes["class"] }>(),
+    {
+        modelValue: 0,
+    },
+)
 
-const delegatedProps = reactiveOmit(props, 'class')
+const delegatedProps = reactiveOmit(props, "class")
 </script>
 
 <template>
     <ProgressRoot
         :class="
       cn(
-        'bg-primary/20 relative h-2 w-full overflow-hidden rounded-full',
+        'relative h-2 w-full overflow-hidden rounded-full bg-primary/20',
         props.class,
       )
     "
-        data-slot="progress"
         v-bind="delegatedProps"
     >
         <ProgressIndicator
+            class="h-full w-full flex-1 bg-primary transition-all"
             :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`"
-            class="bg-primary h-full w-full flex-1 transition-all"
-            data-slot="progress-indicator"
         />
     </ProgressRoot>
 </template>
