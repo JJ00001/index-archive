e14f83c272c60bc102867935db4af47cd5bff5ab
Julien Guntermann <julienguntermann@web.de>
2026-03-25 21:13:10 +0100

    feat: add strict typings to useDataTableSorting

--- resources/js/composables/useDataTableSorting.ts
+++ resources/js/composables/useDataTableSorting.ts
@@ -1,17 +1,18 @@
-import { ref } from 'vue'
+import type {ColumnSort, SortingState, Updater} from '@tanstack/vue-table'
+import {ref} from 'vue'
 
-export default function useDataTableSorting (sort, visitRoute) {
-    const sorting = ref([])
+export default function useDataTableSorting(
+    sort: unknown,
+    visitRoute: (params?: object, options?: { onFinish?: () => void }) => void,
+) {
+    const sorting = ref<SortingState>([])
     const isSorting = ref(false)
 
-    const resolveNextSorting = (updater) => {
-        const previousSorting = sorting.value
-        const updatedSorting = updater(previousSorting)
-
-        return updatedSorting ?? []
+    const resolveNextSorting = (updater: Updater<SortingState>): SortingState => {
+        return typeof updater === 'function' ? updater(sorting.value) : updater
     }
 
-    const requestSorting = (columnSort) => {
+    const requestSorting = (columnSort: ColumnSort): void => {
         const sortParam = columnSort.desc ? `-${columnSort.id}` : columnSort.id
 
         isSorting.value = true
@@ -26,7 +27,7 @@ export default function useDataTableSorting (sort, visitRoute) {
         )
     }
 
-    const requestDefaultSorting = () => {
+    const requestDefaultSorting = (): void => {
         isSorting.value = true
 
         visitRoute(
@@ -39,7 +40,7 @@ export default function useDataTableSorting (sort, visitRoute) {
         )
     }
 
-    const handleSortingChange = (updater) => {
+    const handleSortingChange = (updater: Updater<SortingState>): void => {
         sorting.value = resolveNextSorting(updater)
 
         const columnSort = sorting.value[0]
