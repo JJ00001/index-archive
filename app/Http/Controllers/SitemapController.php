e8e023a9769aff792a5d21524de34fd1b7e3a95d
Julien Guntermann <julienguntermann@web.de>
2026-03-28 19:09:15 +0100

    feat: add `SitemapController` and route for dynamic sitemap generation

---
+++ app/Http/Controllers/SitemapController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use App\Models\Index;
+use Spatie\Sitemap\Sitemap;
+
+class SitemapController extends Controller
+{
+
+    public function __invoke(): Sitemap
+    {
+        return Sitemap::create()
+                      ->add(url('/'))
+                      ->add(route('companies.index'))
+                      ->add(route('indices.index'))
+                      ->add(Company::query()->select(['id', 'updated_at', 'created_at'])->cursor())
+                      ->add(Index::query()->select(['id', 'updated_at', 'created_at'])->cursor());
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\SitemapController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])->name('home');
+Route::get('/sitemap.xml', SitemapController::class)->name('sitemap');
 
 Route::resource('companies', CompanyController::class)->only(['index', 'show']);
 Route::resource('indices', IndexController::class)->only(['index', 'show']);
