2c969c87188923409c8a2258f63f7a4c4c56ad82
Julien Guntermann <julienguntermann@web.de>
2026-03-29 19:47:03 +0200

    feat: add `AllocationWeightBar` component for visualizing weighted allocations

---
+++ resources/js/Components/AllocationWeightBar.vue
@@ -0,0 +1,20 @@
+<script lang="ts"
+        setup>
+import {useAllocationWeight, type WeightedAllocationItem} from '@/composables/useAllocationWeight'
+
+const props = defineProps<{
+    items: WeightedAllocationItem[]
+    weight?: number | string | null
+}>()
+
+const {normalizedWeight} = useAllocationWeight(() => props.items)
+</script>
+
+<template>
+    <div class="relative h-2 w-full overflow-hidden rounded-full bg-muted/50">
+        <div
+            :style="{ width: normalizedWeight(weight) }"
+            class="h-full rounded-full bg-foreground/80 transition-[width]"
+        />
+    </div>
+</template>
