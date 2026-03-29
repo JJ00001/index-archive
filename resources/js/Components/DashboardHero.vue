0e8ba73776e3578bd02dadf64c86683d857352e3
Julien Guntermann <julienguntermann@web.de>
2026-03-29 19:43:42 +0200

    refactor: adjust font weight and remove custom font in `DashboardHero` component

--- resources/js/Components/DashboardHero.vue
+++ resources/js/Components/DashboardHero.vue
@@ -3,10 +3,10 @@
 
 <template>
     <section class="mx-auto my-20 max-w-3xl space-y-12 text-center">
-        <h1 class="font-apple-garamond text-5xl tracking-tight text-slate-900 sm:text-6xl md:text-7xl">
+        <h1 class="text-5xl font-medium tracking-tight text-slate-900 sm:text-6xl md:text-7xl">
             Explore how global markets evolve
         </h1>
-        <h2 class="font-apple-garamond text-2xl leading-relaxed text-slate-600">
+        <h2 class="text-2xl leading-relaxed text-slate-600">
             The composition of major market indices constantly changes.
             <br>
             IndexArchive uncovers shifts across countries, sectors, and companies to understand what’s really driving
