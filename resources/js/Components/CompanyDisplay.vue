d01bf53a7f4b62c2f6101e1f6a150427731e4d76
Julien Guntermann <julienguntermann@web.de>
2026-03-29 12:39:20 +0200

    feat: migrate `CompanyDisplay` component to TypeScript

--- resources/js/Components/CompanyDisplay.vue
+++ resources/js/Components/CompanyDisplay.vue
@@ -1,24 +1,25 @@
-<script setup>
+<script lang="ts"
+        setup>
 import CompanyLogo from '@/Components/CompanyLogo.vue'
+import type {Company} from '@/interfaces/Company.ts'
 
-defineProps({
-  company: {
-    type: Object,
-    required: true,
-  },
-})
+defineProps<{
+    company: Company
+}>()
 </script>
 
 <template>
-  <div class="flex items-center">
-      <div class="w-16 h-8 mr-4 shrink-0">
-      <CompanyLogo :logo-path="company.logo"
-                   class="w-full h-full" />
+    <div class="flex items-center">
+        <div class="mr-4 h-8 w-16 shrink-0">
+            <CompanyLogo
+                :logo-path="company.logo"
+                class="h-full w-full"
+            />
+        </div>
+        <div>
+            <span class="font-bold">{{ company.name }}</span>
+            <br>
+            <span class="text-sm text-muted-foreground">{{ company.ticker }}</span>
+        </div>
     </div>
-    <div>
-      <span class="font-bold">{{ company.name }}</span>
-      <br>
-      <span class="text-sm text-muted-foreground">{{ company.ticker }}</span>
-    </div>
-  </div>
 </template>
